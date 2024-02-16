014e89c1bc747b627d05932b818f4cdd718600cf
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-16 14:14:13 +0900

    2024_02_14_14:44

--- Daily Task/2024_02_13/미로_2.py
+++ Daily Task/2024_02_13/미로_2.py
@@ -15,18 +15,20 @@ for test_case in range(1,T+1):
     di = [1,-1,0,0]
     dj = [0,0,1,-1]
     result = 0
-    stack = [start_end[0]]
+    stack = [start_end[0]] # 시작 좌표
     while stack:
         r,c = stack.pop()
         
         for k in range(4):    
-            if 0 <= r+di[k] < n and 0 <= c+dj[k] < n:
+            if 0 <= r+di[k] < n and 0 <= c+dj[k] < n: # index range 체크
+                
                 if maze[r+di[k]][c+dj[k]] == '0':
                     stack.append((r+di[k],c+dj[k]))
-                    maze[r+di[k]][c+dj[k]] = '1'
+                    maze[r+di[k]][c+dj[k]] = '1' # visited
+                
                 elif maze[r+di[k]][c+dj[k]] == '3':
-                    result = 1
-                    stack = []
+                    result = 1 # 도착했을 때의 출력
+                    stack = [] # while문 탈출을 위해
                     break
     
     print(f'#{test_case} {result}')
