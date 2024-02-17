97d26aa2afc57cffc4f6fdd1bd37752f9f9881ca
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-17 18:42:31 +0900

    SWEA 괄호 CLEAR!! 백준 괄호 풀어야지!

--- Daily Task/2024_02_07/괄호검사_retry_retry.py
+++ Daily Task/2024_02_07/괄호검사_retry_retry.py
@@ -4,4 +4,19 @@ for test_case in range(1,T+1):
    
     stack = []
     for s in string:
-        pass
+        if s in '{(':
+            stack.append(s)
+        elif s in '})':
+            if s == '}' and stack and stack[-1] == '{':
+                stack.pop()
+            elif s == ')' and stack and stack[-1] == '(':
+                stack.pop()
+
+            # 필요한 이유 -> (})
+            else:
+                stack.append(s) 
+        
+    if stack:
+        print(f'#{test_case} {0}')
+    else:
+        print(f'#{test_case} {1}')
