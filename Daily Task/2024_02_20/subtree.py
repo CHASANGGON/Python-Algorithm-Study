ad609f14e1558f6949582afa2f15193c55d6eb75
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-20 20:55:14 +0900

    밥 먹었으면, 밥 값은 해야지...

--- Daily Task/2024_02_20/subtree.py
+++ Daily Task/2024_02_20/subtree.py
@@ -4,13 +4,26 @@ for test_case in range(1,T+1):
     E, N = map(int,input().split())
     arr = list(map(int,input().split()))
     
-    left = [0]*(E+1)
-    right = [0]*(E+1)
+    # 간선 정보를 담을 리스트 생성
+    # 노드 번호는 1번부터 E+1번까지 존재
+    left = [0]*(E+2)
+    right = [0]*(E+2)
     
+    # 간선 정보 입력
     for i in range(E):
-        left[i]
+        if left[arr[2*i]]:
+            right[arr[2*i]] = arr[2*i+1]
+        else:
+            left[arr[2*i]] = arr[2*i+1]
     
-    cnt = 0
+    stack = [right[N],left[N]]
     
+    cnt = 1
+    while stack:
+        next_node = stack.pop(0)
+        if next_node:
+            cnt += 1
+            stack.append(left[next_node])
+            stack.append(right[next_node])
     
     print(f'#{test_case} {cnt}')
