592c13d2c5650edf8d7efad61e39d17f356e5edd
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-21 19:30:26 +0900

    IM 대비 문제 끝~

--- 3. IM 대비/농작물 수확하기.py
+++ 3. IM 대비/농작물 수확하기.py
@@ -1,6 +1,3 @@
-
-        
-
 t = int(input())
 
 for tc in range(1,1+t):
@@ -8,5 +5,21 @@ for tc in range(1,1+t):
     
     farm = [list(map(int,list(input()))) for _ in range(n)]
     
-    for i in range(n//2):
-        
+    profit = farm[n//2][n//2]
+    farm[n//2][n//2] = -1
+    q = [[n//2,n//2]]
+    di = [1,-1,0,0]
+    dj = [0,0,1,-1]
+    
+    for _ in range(n//2):
+        next_q = []
+        while q:
+            i,j = q.pop()
+            for k in range(4):
+                if farm[i+di[k]][j+dj[k]] != -1:
+                   profit += farm[i+di[k]][j+dj[k]]
+                   farm[i+di[k]][j+dj[k]] = -1
+                   next_q.append([i+di[k],j+dj[k]])
+        q = next_q[:]
+
+    print(f'#{tc} {profit}')
