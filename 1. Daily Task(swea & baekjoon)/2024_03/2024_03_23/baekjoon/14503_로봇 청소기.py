6226f962ef63f7e0779c5c27005156d48633d21a
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-03-24 01:35:46 +0900

    ?

--- 1. Daily Task(swea & baekjoon)/2024_03/2024_03_23/baekjoon/14503_로봇 청소기.py
+++ 1. Daily Task(swea & baekjoon)/2024_03/2024_03_23/baekjoon/14503_로봇 청소기.py
@@ -2,35 +2,31 @@ def dfs():
     global cnt
     
     while stack:
-        i, j, d = stack.pop()
-        if arr[i][j] == 0:
-            arr[i][j] = -1
-            cnt += 1
+        i, j, origin_d = stack.pop()
+        if arr[i][j] == 0: # 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
+            arr[i][j] = -1 # -1은 청소 완료의 의미
+            cnt += 1 # 카운트
             
         # 인접 칸 탐색
-        is_blank = False
-        origin_d = d
-        for offset_d in range(4):
-            d = (d - offset_d) % 4
+        is_blank = False # 빈칸이 있는지 확인할 변수
+        for offset_d in range(1,5): # 반시계 방향으로 90도 회전
+            d = (origin_d - offset_d) % 4
             ni = i + di[d]
             nj = j + dj[d]
-            if 0 <= ni < n - 1 and 0 <= nj < m - 1 and arr[ni][nj] == 0:
+            if arr[ni][nj] == 0:
                 stack.append([ni, nj, d])
                 is_blank = True
-                arr[ni][nj] = -1
-                cnt += 1
                 break # 청소되지 않은 빈 칸이 있는 경우, 앞으로 한 칸 전진 -> break
             
         # 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
         if not is_blank:
             # 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진
-            ni = i - di[origin_d]
+            ni = i - di[origin_d] # 바라보는 방향의 역방향 = 값을 빼주기
             nj = j - dj[origin_d]
-            if 0 <= ni < n - 1 and 0 <= nj < m - 1 and arr[ni][nj] == 0:
-                stack.append([ni,nj,origin_d])
-            # 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
-            else:
+            if arr[ni][nj] == 1: # 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
                 return
+            else:
+                stack.append([ni,nj,origin_d])
 
 import sys
 input = sys.stdin.readline
--- 6. 미완성/test.py
+++ 6. 미완성/test.py
@@ -1,23 +1,4 @@
-# 선택 정렬
-T = int(input())
-
-for test_case in range(1,1+T):
-    N = int(input())
-
-    lst = list(map(int,input().split()))
-
-    for i in range(N-1):
-        m = i
-        for j in range(i,N):
-            if i % 2 == 0:
-                if lst[j] > lst[m]:
-                    m = j
-            else:
-                if lst[j] < lst[m]:
-                    m = j
-        
-        lst[i], lst[m] = lst[m], lst[i]
-    print(f'#{test_case} ',end='')
-    for i in range(10):
-        print(lst[i], end=' ')
-    print()
+origin_d = 315
+for offset_d in range(1,5): # 반시계 방향으로 90도 회전
+    d = (origin_d - offset_d) % 4
+    print(d)
