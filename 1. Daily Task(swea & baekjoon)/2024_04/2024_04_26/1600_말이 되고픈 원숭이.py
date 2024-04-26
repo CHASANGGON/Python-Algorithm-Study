9e5e8afd961433444cb4720a5626834b0a29f236
CHASANGGON <yg9618@naver.com>
2024-04-26 18:01:25 +0900

    2024_04_26

---
+++ 1. Daily Task(swea & baekjoon)/2024_04/2024_04_26/1600_말이 되고픈 원숭이.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+w, h = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(h)]
+ans = -1
+
+dq = deque()
+dq.append((0,0,k))
+
+
+while dq:
+    i,j,remain_k = dq.popleft()
+    
+    if remain_k: # 말처럼 이동 가능하면
+        for ni, nj in (i+2,j+1), (i+2,j-1), (i-2,j+1), (i-2,j-1), (i+1,j+2), (i+1,j-2), (i-1,j+2), (i-1,j-2):
+            if 0 <= ni < h and 0 <= nj < w and arr[ni][nj] == 0: # 인덱스 검사 및 방문 체크
+                arr[ni][nj] = arr[i][j] + 1
+                dq.append([ni,nj,remain_k-1]) # remain_k 1 감소
+    
+    for ni, nj in (i+1,j), (i-1,j), (i,j+1), (i,j-1): # 그냥 이동
+        if 0 <= ni < h and 0 <= nj < w and arr[ni][nj] == 0: # 인덱스 검사 및 방문 체크
+            arr[ni][nj] = arr[i][j] + 1
+            dq.append([ni,nj,remain_k]) # remain_k 감소 x
+            
+    if arr[h-1][w-1]:
+        ans = arr[h-1][w-1]
+        break
+
+print(ans)
+
