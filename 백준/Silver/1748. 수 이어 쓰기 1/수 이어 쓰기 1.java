58b2168ca2783ecbd9fc203d4d88b2ed22d8dcbe
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-12-22 14:46:44 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -5,27 +5,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        if (n < 10) {
+            System.out.println(n);
+        } else {
 
-        // 1     ->     9        : 9    * 1 = 9
-        // 10    ->     99       : 90   * 2 = 180
-        // 100    ->    999      : 900  * 3 = 2700
+            // 1     ->     9        : 9    * 1 = 9
+            // 10    ->     99       : 90   * 2 = 180
+            // 100    ->    999      : 900  * 3 = 2700
 
-        // n = 55
-        // i = 1, a = 9 -> ans = 9
-        // b = n - a = 55 - 9 = 46
-        int ans = 0;
-        int powN = 9;
-        int c = 0;
-        int d = 0;
-        for (int i = 0; i < String.valueOf(n).length(); i++) {
-            powN = 9 * (int) Math.pow(10, i - 1);
-            ans += powN * i;
-            c = n - ((int) Math.pow(10, i) - 1);
-            d = i + 1;
+            // n = 55
+            // i = 1, a = 9 -> ans = 9
+            // b = n - a = 55 - 9 = 46
+            int ans = 0;
+            int powN = 9;
+            int c = 0;
+            int d = 0;
+            for (int i = 1; i < String.valueOf(n).length(); i++) {
+                powN = 9 * (int) Math.pow(10, i - 1);
+                ans += powN * i;
+                c = n - ((int) Math.pow(10, i) - 1);
+                d = i + 1;
+            }
+
+            ans += c * d;
+
+            System.out.println(ans);
         }
-
-        ans += c * d;
-
-        System.out.println(ans);
     }
 }
