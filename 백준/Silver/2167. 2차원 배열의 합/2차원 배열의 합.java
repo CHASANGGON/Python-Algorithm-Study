a26d172f2e8ba0b4b8a2f0d08f7f35215452f7af
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-12-25 23:43:34 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1932 ms, Memory: 35472 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -3,33 +3,40 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] NM = br.readLine().split(" ");
-        int N = Integer.parseInt(NM[0]);
-        int M = Integer.parseInt(NM[1]);
-        int[][] arr = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            String[] inputs = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(inputs[j]);
+        
+        // 배열 크기 입력
+        String[] dimensions = br.readLine().split(" ");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
+        
+        // 2차원 배열 입력
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            String[] rowValues = br.readLine().split(" ");
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = Integer.parseInt(rowValues[j]);
             }
         }
+        
+        // 쿼리 개수 입력
+        int queryCount = Integer.parseInt(br.readLine());
+        
+        // 쿼리 처리
+        for (int q = 0; q < queryCount; q++) {
+            String[] rangeQuery = br.readLine().split(" ");
+            int startRow = Integer.parseInt(rangeQuery[0]) - 1;
+            int startCol = Integer.parseInt(rangeQuery[1]) - 1;
+            int endRow = Integer.parseInt(rangeQuery[2]) - 1;
+            int endCol = Integer.parseInt(rangeQuery[3]) - 1;
 
-        int K = Integer.parseInt(br.readLine());
-
-        for (int k = 0; k < K; k++) {
-            String[] IJXY = br.readLine().split(" ");
-            int I = Integer.parseInt(IJXY[0]);
-            int J = Integer.parseInt(IJXY[1]);
-            int X = Integer.parseInt(IJXY[2]);
-            int Y = Integer.parseInt(IJXY[3]);
-            int ans = 0;
-            for (int i = I - 1; i <= X - 1; i++) {
-                for (int j = J - 1; j <= Y - 1; j++) {
-                    ans += arr[i][j];
+            // 구간 합 계산
+            int sum = 0;
+            for (int i = startRow; i <= endRow; i++) {
+                for (int j = startCol; j <= endCol; j++) {
+                    sum += matrix[i][j];
                 }
             }
-            System.out.println(ans);
+            System.out.println(sum);
         }
     }
 }
