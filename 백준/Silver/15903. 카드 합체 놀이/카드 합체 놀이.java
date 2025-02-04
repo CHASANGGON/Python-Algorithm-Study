3d35e418a16cb6115df25559abd7c0472489c308
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-02-04 19:28:21 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 144 ms, Memory: 15188 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -7,6 +7,12 @@ public class Main {
 
         // n, m 입력 받기
         String[] nm = br.readLine().split(" ");
+        // 1 ≤ ai ≤ 1,000,000 이라서 long을 사용
+        // 0 ≤ m ≤ 15,000 = 15×n
+        // n = 1000 이고,
+        // 999,000 <= ai <= 1,000,000 이라면
+        // 1,000,000 * 2(합한 결과) * 15,000(합하는 횟수) = 30,000,000,000 = 300억
+        // 따라서 합하는 과정에서 int 범위 21억을 초과할 가능성이 있음
         long n = Integer.parseInt(nm[0]);
         long m = Integer.parseInt(nm[1]);
 
@@ -38,14 +44,26 @@ public class Main {
         // 6 6 7 7 11 11
         // = 48 = 37 + 11
 
+        // 제일 작은 두 수를 골라서(우선순위큐에서 추출)
+        // 합한 값으로 덮어쓰기(우선순위큐에 삽입)
+
         // m 번 합
+        // 우선 순위 큐의 시간 복잡도는 O(logn)
         for (int i = 0; i < m; i++) {
-            long a = pq.poll();
-            long b = pq.poll();
+            long a = pq.poll(); // O(logn)
+            long b = pq.poll(); // O(logn)
             long ab = a + b;
-            pq.offer(ab);
-            pq.offer(ab);
+            pq.offer(ab); // O(logn)
+            pq.offer(ab); // O(logn)
         }
+
+        // 따라서 총 연산의 횟수는 O(mlogn)
+        // 최대값인 경우에 (입력 연산은 O(1)이므로 고려하지 않음)
+        // n = 1,000
+        // m = 15,000
+        // 총 15,000 * log(1000) = 15,000 * 10(대략)
+        // = 150,000(0.0015초 = 1.5ms) < 100,000,000(1억) = 1초
+        // 따라서 충분히 통과 가능하다!
 
         long ans = 0;
         while (!pq.isEmpty()) {
