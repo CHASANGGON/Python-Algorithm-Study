4522e3cbc19ac74d1c1adfbdb6c92c4b276484f6
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-02-23 23:11:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 532 ms, Memory: 52340 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -5,37 +5,32 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // N 입력 받기
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 배열 입렫 받기
-        int[] arr = new int[N];
+        // 배열 입력: dp 배열에 누적합으로 바로 저장
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // DP(누적합)
-        int[] dp = new int[N];
-        dp[0] = arr[0];
-        for (int i = 1; i < N; i++) {
-            dp[i] = dp[i - 1] + arr[i];
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            dp[i] = dp[i - 1] + input;
         }
 
 
         // 출력
         for (int k = 0; k < M; k++) {
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken()) - 2;
-            int j = Integer.parseInt(st.nextToken()) - 1;
-            if (i == -1) {
-                System.out.println(dp[j]);
-            } else {
-                System.out.println(dp[j] - dp[i]);
-            }
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int sum = dp[j] - dp[i - 1];
+            bw.write(sum + "\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 }
