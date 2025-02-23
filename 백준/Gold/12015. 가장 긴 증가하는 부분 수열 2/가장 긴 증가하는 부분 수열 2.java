626610c1fb01c7a85264204e1636ea1c00fad6bf
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-02-23 22:05:07 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 600 ms, Memory: 121124 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -2,51 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] A;
-    static ArrayList<Integer> LIS = new ArrayList<>();
+    private static int N;
+    private static int[] arr;
+    private static List<Integer> LIS = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // N 입력 받기
-        N = Integer.parseInt(br.readLine());
-        A = new int[N]; // 배열 생성
+        int N = Integer.parseInt(br.readLine());
 
-        // 배열 입력 받기
+        // 배열 입렫 받기
+        arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // LIS 배열을 유지하면서 처리
-        for (int num : A) {
-            int idx = lowerBound(LIS, num);
+        // 배열을 모두 순회하며 LIS에 들어갈 자리 찾기
+        for (int target : arr) {
+            int idx = findIndex(target);
 
-            if (idx == LIS.size()) { // 가장 큰 값이면 배열의 끝에 추가
-                LIS.add(num);
-            } else { // lower_bound 위치에 있는 값을 교체
-                LIS.set(idx, num);
+            if (idx == LIS.size()) { // 들어갈 자리가 없다면 끝에 추가
+                LIS.add(target);
+            } else {
+                LIS.set(idx, target); // 해당 인덱스의 값을 덮어쓰기
             }
         }
 
-        // 출력
-        System.out.println(LIS.size()); // LIS 배열의 크기가 정답
+        // LIS 배열의 길이가 정답
+        System.out.println(LIS.size());
     }
 
-    /** 입력된 LIS 배열을 이분 탐색해서 값이 들어갈 인덱스를 반환해주는 함수 (lower_bound) **/
-    private static int lowerBound(ArrayList<Integer> arr, int target) {
-        int left = 0, right = arr.size();
+    private static int findIndex(int target) {
+        int left = 0, right = LIS.size() - 1;
 
-        while (left < right) {
+        while (left <= right) {
             int mid = (left + right) / 2;
-            
-            if (arr.get(mid) >= target) {
-                right = mid;
+
+            if (target <= LIS.get(mid)) {
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
+
         }
-        return left; // target이 들어갈 위치 반환
+
+        return left;
     }
 }
