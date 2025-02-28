06d61074e01d7deaf5a6931092777127387316c5
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-03-01 03:37:15 +0900

    [Silver II] Title: 제곱수의 합, Time: 144 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -10,37 +10,19 @@ public class Main {
 
         // dp 배열 생성
         int[] dp = new int[N + 1];
+        dp[1] = 1; // 초기값 설정
 
         // dp
-        // 제곱수는 모두 1
-        for (int i = 1; i <= (int) Math.sqrt(N); i++) {
-            dp[i * i] = 1;
-        }
-
-        // 제곱수들의 합
-        for (int i = 1; i < (int) Math.sqrt(N); i++) {
-            for (int j = 1; j < (int) Math.sqrt(N); j++) {
-                int target = i * i + j * j;
-                if (target <= N && dp[target] == 0) {
-                    dp[target] = 2;
+        for (int i = 2; i <= N; i++) {
+            if (i == (int) Math.sqrt(i) * Math.sqrt(i)) { // 제곱수는 1
+                dp[i] = 1;
+            } else { // 제곱수가 아니면 제곱수의 합으로 표현
+                dp[i] = i; // 최악의 경우로 초기화
+                for (int j = 1; j * j < i; j++) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
                 }
             }
         }
-
-        // 탐색
-        for (int i = 3; i <= N; i++) {
-            if (dp[i] == 0) {
-                dp[i] = Integer.MAX_VALUE;
-                for (int j = 1; j <= (i + 1) / 2; j++) {
-                    dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
-                }
-            }
-        }
-
-        // 디버깅
-//        for (int i = 1; i <= N; i++) {
-//            System.out.print(dp[i] + " ");
-//        }
 
         // 출력
         System.out.println(dp[N]);
