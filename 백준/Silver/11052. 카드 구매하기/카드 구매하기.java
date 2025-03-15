d51e69f18dda3a384ff8a7c2fe91edafcbf83d41
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-03-15 21:10:01 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // N 입력 받기
+        int N = Integer.parseInt(br.readLine());
+
+        // Pi 입력 받기
+        int[] dp = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            dp[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // dp
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j + i <= N; j++) {
+                dp[j + i] = Math.max(dp[j + i], dp[i] + dp[j]);
+            }
+            for (int j = 1; i * j < N + 1; j++) {
+                dp[j * i] = Math.max(dp[j * i], dp[i] * j);
+            }
+
+        }
+
+        System.out.println(dp[N]);
+    }
+}
