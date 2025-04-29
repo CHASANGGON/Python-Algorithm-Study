db63af3b3de5b4cb3902395e854143c3444eddd3
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-04-29 19:37:32 +0900

    [Gold III] Title: 파일 합치기, Time: 424 ms, Memory: 28668 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -22,13 +22,16 @@ public class Main {
             }
 
             // DP
-            for (int length = 2; length <= K; length++) {
-                for (int start = 1; start <= K - length + 1; start++) {
+            for (int length = 2; length <= K; length++) { // 길이를 증가시키면 답을 만들어갈 것임(이게 DP 방식)
+                for (int start = 1; start <= K - length + 1; start++) { // start ~ end 구간을 하나씩 갱신
                     int end = start + length - 1;
 
-                    dp[start][end] = Integer.MAX_VALUE;
+                    dp[start][end] = Integer.MAX_VALUE; // 갱신 대상
 
-                    for (int divLine = start; divLine < end; divLine++) {// 자르는 기준 점
+                    for (int divLine = start; divLine < end; divLine++) { // (start ~ end)에서 자르는 기준 점
+                        // 길이 4를 예로 들면, 
+                        // (1,3) or (2,2) or (3,1) 로 잘라서 합친 것 중에서 
+                        // 최댓값으로 갱신
                         dp[start][end] = Math.min(dp[start][end], dp[start][divLine] + dp[divLine + 1][end] + sum[end] - sum[start - 1]);
                     }
                 }
