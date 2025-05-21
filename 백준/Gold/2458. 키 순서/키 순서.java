08b88bc60f75a6cac9f3ed4bf1c8f2e0f027cf7d
차상곤 <yg9618@naver.com>
2025-05-21 20:16:26 +0900

    [Gold IV] Title: 키 순서, Time: 492 ms, Memory: 40032 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] result;
+    private static boolean[] visited;
+    private static List<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N(노드의 개수) 입력 받기
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 그래프 생성(N (2 ≤ N ≤ 500) -> ArrayList에 저장)
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 간선 정보 입력 받기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph[start].add(end); // 단방향 그래프
+        }
+
+        // dfs
+        result = new int[N+1];
+        for(int start = 1; start <= N; start++) {
+            visited = new boolean[N + 1];
+            visited[start] = true;
+            dfs(start, start);
+        }
+
+        // 검사
+        int ans = 0;
+        for(int i = 1; i <= N; i++) {
+            if (result[i] == N-1) ans++;
+        }
+
+        // 출력
+        System.out.println(ans);
+    }
+
+    private static void dfs(int root, int now) {
+        for(int next: graph[now]) {
+            if(!visited[next]) {
+                result[root]++;
+                result[next]++;
+                visited[next] = true;
+                dfs(root, next);
+            }
+        }
+    }
+}
