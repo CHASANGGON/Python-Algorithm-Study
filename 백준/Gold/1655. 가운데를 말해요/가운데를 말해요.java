6e1feab062dbe0dd984f8d2c496121f00492f098
차상곤 <yg9618@naver.com>
2025-05-23 13:03:18 +0900

    [Gold II] Title: 가운데를 말해요, Time: 424 ms, Memory: 33388 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -14,18 +14,18 @@ public class Main {
         PriorityQueue<Integer> big = new PriorityQueue<>();
 
         while (N-- > 0) {
-            small.add(Integer.parseInt(br.readLine()));
+            small.add(Integer.parseInt(br.readLine())); // 일단 small에 넣기
 
-            if (!big.isEmpty() && small.peek() > big.peek()) {
-                small.add(big.poll());
+            if (!big.isEmpty() && small.peek() > big.peek()) { // 보정
+                small.add(big.poll()); // swap
                 big.add(small.poll());
             }
 
-            if (small.size() > big.size() + 1) {
+            if (small.size() > big.size() + 1) { // small이 한 개 더 많아야함
                 big.add(small.poll());
             }
 
-            sb.append(small.peek()).append("\n");
+            sb.append(small.peek()).append("\n"); // 항상 small의 peek를 출력
         }
 
         System.out.println(sb.toString());
