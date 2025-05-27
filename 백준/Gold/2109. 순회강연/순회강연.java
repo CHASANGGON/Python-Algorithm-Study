1d8fc0e364caa5fac4adc5c82e8b96660c9485a5
차상곤 <yg9618@naver.com>
2025-05-28 05:58:55 +0900

    [Gold III] Title: 순회강연, Time: 268 ms, Memory: 20364 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,41 +1,49 @@
 import java.io.*;
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
+    private static int N;
+    private static int[][] lectures;
+    ;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[][] lectures = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
+        // 입력
+        N = Integer.parseInt(br.readLine());
+        lectures = new int[N][2];
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            lectures[i][0] = Integer.parseInt(st.nextToken()); // p: 강연료
-            lectures[i][1] = Integer.parseInt(st.nextToken()); // d: 마감일
+            lectures[i][0] = Integer.parseInt(st.nextToken()); // 강연료(fee)
+            lectures[i][1] = Integer.parseInt(st.nextToken()); // 기한(deadline)
         }
 
-        // 마감일 기준 오름차순 정렬
-        Arrays.sort(lectures, (a, b) -> a[1] - b[1]);
+        Arrays.sort(lectures, (a, b) -> a[1] - b[1]); // deadline 기준 오름차순 정렬
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // min-heap
+        // 디버깅
+//        for (int i = 0; i < N; i++) {
+//            System.out.println(lectures[i][0] + " " + lectures[i][1]);
+//        }
+//        System.out.println();
 
-        for (int i = 0; i < n; i++) {
-            int pay = lectures[i][0];
-            int day = lectures[i][1];
-
-            if (pq.size() < day) {
-                pq.offer(pay); // 날짜 여유 있으면 그냥 추가
-            } else if (!pq.isEmpty() && pq.peek() < pay) {
-                pq.poll(); // 가장 값싼 강연 제거
-                pq.offer(pay); // 지금 강연 추가
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 강의료를 저장할 pq
+        int lectureIdx = 0;
+        while (lectureIdx < N) {
+            if (pq.size() < lectures[lectureIdx][1]) { // day 보다 pq가 작다면 그냥 추가
+                pq.add(lectures[lectureIdx][0]); // 강의료 추가
+            } else if (pq.peek() < lectures[lectureIdx][0]) { // 이 강의가 기존 강의보다 강의료가 크다면
+                pq.poll();
+                pq.add(lectures[lectureIdx][0]); // 현재 강의로 대체
             }
+            lectureIdx++;
         }
 
-        int result = 0;
-        while (!pq.isEmpty()) {
-            result += pq.poll();
+        // 출력
+        int totalFee = 0;
+        for (int fee : pq) {
+            totalFee += fee;
         }
-
-        System.out.println(result);
+        System.out.println(totalFee);
     }
 }
