0c96d9e6b80042383aeb67847951d48637260f40
차상곤 <yg9618@naver.com>
2025-05-29 12:31:56 +0900

    [Gold III] Title: 순회강연, Time: 252 ms, Memory: 20712 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,49 +1,44 @@
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
-    private static int N;
-    private static int[][] lectures;
-    ;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 입력
-        N = Integer.parseInt(br.readLine());
-        lectures = new int[N][2];
+        int N = Integer.parseInt(br.readLine());
+        int[][] tasks = new int[N][2];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            lectures[i][0] = Integer.parseInt(st.nextToken()); // 강연료(fee)
-            lectures[i][1] = Integer.parseInt(st.nextToken()); // 기한(deadline)
+            tasks[i][0] = Integer.parseInt(st.nextToken()); // d(과제 점수)
+            tasks[i][1] = Integer.parseInt(st.nextToken()); // w(과제 마감일)
         }
 
-        Arrays.sort(lectures, (a, b) -> a[1] - b[1]); // deadline 기준 오름차순 정렬
+        Arrays.sort(tasks, Comparator.comparingInt(a -> a[1]));
 
-        // 디버깅
 //        for (int i = 0; i < N; i++) {
-//            System.out.println(lectures[i][0] + " " + lectures[i][1]);
+//            System.out.println(tasks[i][0] + " " + tasks[i][1]);
 //        }
-//        System.out.println();
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 강의료를 저장할 pq
-        int lectureIdx = 0;
-        while (lectureIdx < N) {
-            if (pq.size() < lectures[lectureIdx][1]) { // day 보다 pq가 작다면 그냥 추가
-                pq.add(lectures[lectureIdx][0]); // 강의료 추가
-            } else if (pq.peek() < lectures[lectureIdx][0]) { // 이 강의가 기존 강의보다 강의료가 크다면
-                pq.poll();
-                pq.add(lectures[lectureIdx][0]); // 현재 강의로 대체
+        // Greedy
+        int idx = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // pq에 과제 점수를 저장(오름차순)
+        while (idx < N) {
+            if (pq.size() < tasks[idx][1]) { // 현재 과제 마감일보다 pq의 크기가 작다면
+                pq.add(tasks[idx][0]); // 추가
+            } else if (pq.peek() < tasks[idx][0]) { // 기존 과제 점수보다 현재 과제 점수가 더 크몀ㄴ
+                pq.poll(); // 교체
+                pq.add(tasks[idx][0]); // 교체
             }
-            lectureIdx++;
+
+            idx++;
         }
 
         // 출력
-        int totalFee = 0;
-        for (int fee : pq) {
-            totalFee += fee;
+        int ans = 0;
+        while (!pq.isEmpty()) {
+            ans += pq.poll();
         }
-        System.out.println(totalFee);
+        System.out.println(ans);
     }
 }
