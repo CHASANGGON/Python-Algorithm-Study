3ad20157b678e8f9068c5f84ea028ba61efd73ab
차상곤 <yg9618@naver.com>
2025-05-30 14:02:04 +0900

    [Gold V] Title: 행복 유치원, Time: 480 ms, Memory: 53516 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -10,22 +10,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력
+        // N, K 입력
         int N = Integer.parseInt(st.nextToken()); // 원생의 수
         int K = Integer.parseInt(st.nextToken()); // 조의 개수
 
+        // 키 입력(입력 받으면서 차이를 바로 계산)
         st = new StringTokenizer(br.readLine());
-        PriorityQueue<Integer> diff = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순 정렬
+        
+        // 키 차이를 저장할 pq
         int now = 0, before = Integer.parseInt(st.nextToken()), start = before;
+        PriorityQueue<Integer> diff = new PriorityQueue<>(Collections.reverseOrder());
+        
+        // 차이를 바로 계산
         for (int i = 0; i < N - 1; i++) {
             now = Integer.parseInt(st.nextToken());
+            
             diff.offer(now - before);
+            
             before = now;
         }
 
-        int ans = before - start;
+        // 최소 비용을 계산
+        int ans = before - start; // 전체 구간
         while(K -- > 1) {
-            ans -= diff.poll();
+            ans -= diff.poll(); // 전체 구간에서 키 차이가 가장 큰 (K-1)개의 구간을 빼기
         }
 
         // 출력
