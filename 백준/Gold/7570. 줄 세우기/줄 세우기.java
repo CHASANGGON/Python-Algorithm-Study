e11da4146ec216b56e7d1e79f1025402d452ab8d
차상곤 <yg9618@naver.com>
2025-06-19 10:43:35 +0900

    [Gold II] Title: 줄 세우기, Time: 428 ms, Memory: 95124 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -23,12 +23,12 @@ public class Main {
         int currentLen = 1;
 
         // 가장 긴 증가하는 "연속된" 부분 수열의 길이 찾기
-        for (int idx = 1; idx < N; idx++) { // 시작점 고정
+        for (int num = 1; num < N; num++) { // 시작점 고정
             // num과 num+1이 연속으로 배치되어 있는지 확인
-            if (position[idx] < position[idx + 1]) {
-                currentLen++;
+            if (position[num] < position[num + 1]) { // num보다 1 큰 값의 인덱스가 더 크다면
+                currentLen++; // 길이 증가
             } else {
-                currentLen = 1;
+                currentLen = 1; // 길이 초기화
             }
             maxLen = Math.max(maxLen, currentLen);
         }
