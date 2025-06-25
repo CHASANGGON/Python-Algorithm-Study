4d50f1f3093d3fa5b24421a3cc05bf5ea9e2722f
차상곤 <yg9618@naver.com>
2025-06-25 20:46:10 +0900

    [Gold II] Title: 줄 세우기, Time: 428 ms, Memory: 91228 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -1,42 +1,35 @@
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
+    private static int[] parent;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // N 입력
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
+        int N = Integer.parseInt(br.readLine()); // 어린이 수 입력
 
-        // 어린이 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] position = new int[N + 1]; // n번 학생의 위치를 저장
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken()); // arr[i] = n
-            position[arr[i]] = i; // position[n] = i
+        int[] indexes = new int[N + 1];
+        for (int index = 0; index < N; index++) {
+            int number = Integer.parseInt(st.nextToken()); // 어린이 번호 입력
+            indexes[number] = index; // 인덱스를 저장
         }
 
-        // 변수 생성
-        int maxLen = 1;
-        int currentLen = 1;
-
-        // 가장 긴 증가하는 "연속된" 부분 수열의 길이 찾기
-        for (int num = 1; num < N; num++) { // 시작점 고정
-            // num과 num+1이 연속으로 배치되어 있는지 확인
-            if (position[num] < position[num + 1]) { // num보다 1 큰 값의 인덱스가 더 크다면
-                currentLen++; // 길이 증가
-            } else {
-                currentLen = 1; // 길이 초기화
+        // 연속 증가 수열 최대 길이 찾기
+        int nowLength = 1;
+        int maxLength = 1;
+        for (int number = 1; number < N; number++) { // 1번부터 N번까지 어린이들의 인덱스를 확인
+            if (indexes[number] < indexes[number + 1]) { // "1 큰 수"의 인덱스가 더 크다면(= "1 큰 수"가 더 뒤에 있는 것)
+                nowLength++; // 길이 증가
+                maxLength = Math.max(maxLength, nowLength); // 기존까지 계산된 최대 길이 갱신
+            } else { // "1 큰 수"가 더 앞에 있는 경우
+                nowLength = 1; // 길이 초기화
             }
-            maxLen = Math.max(maxLen, currentLen);
         }
 
-        // Greedy하게 답을 찾아서 출력
-        // 연속된 수열(길이 maxLen)을 찾았으므로,
-        // Greedy하게 나머지 숫자를 제일 앞뒤로 배치한다면
-        // N(전체 길이) - maxLen 번 만에 구할 수 있음
-        System.out.println(N - maxLen);
+        // 출력
+        // greedy: 최대 연속 길이를 제외한 개수만큼만 차례대로 제일 앞뒤로 보내주면 정렬 가능
+        System.out.println(N - maxLength);
     }
 }
